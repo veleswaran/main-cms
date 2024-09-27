dee3f9e9d48fda1dd46715d8b59e0f2c0fb9fe86
veleswaran <127608442+veleswaran@users.noreply.github.com>
2024-09-27 19:15:07 +0530

    Update database.ts

--- config/database.ts
+++ config/database.ts
@@ -1,6 +1,29 @@
 import path from 'path';
+import { parse } from "pg-connection-string";
 
 export default ({ env }) => {
+   if (env("NODE_ENV") === "production") {
+    const config = parse(process.env.DATABASE_URL);
+    return {
+      defaultConnection: "default",
+      connections: {
+        default: {
+          connector: "bookshelf",
+          settings: {
+            client: "postgres",
+            host: config.host,
+            port: config.port,
+            database: config.database,
+            username: config.user,
+            password: config.password,
+          },
+          options: {
+            ssl: false,
+          },
+        },
+      },
+    };
+  }
   const client = env('DATABASE_CLIENT', 'sqlite');
 
   const connections = {
